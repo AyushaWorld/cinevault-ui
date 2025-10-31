15a65427fd57293b7bfc98258ca230c0890d8176
ayushaWorld <you@example.com>
2025-10-31 16:53:02 +0530

    new updates

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,8 +7,7 @@ const API_BASE_URL = isProduction
   : ""; // Empty string uses Vite proxy in development
 
 const api = axios.create({
-  // baseURL: `${API_BASE_URL}`,
-  baseURL: `https://cinevault-backend-cqbi.onrender.com/api`,
+  baseURL: `${API_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
